b8e6ca24c1ab2f6559500c3ba9a0089b32d1ca89
luistorresdelnuevo <47851991+luistorresdelnuevo@users.noreply.github.com>
2019-05-13 15:21:51 +0200

    Add files via upload

---
+++ Trabajo.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+void presentacion(void);
+void ayuda(void);
+void controles();
+void atacar();
+void asistencia();
+void informacion();
+int azar(int);
+void campeon();
